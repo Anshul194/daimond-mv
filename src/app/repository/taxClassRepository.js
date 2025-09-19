8ac744cdeb8c8ea69139774794e07da243d4651d
tulsisatasiya <tulsisatasiya085@gmail.com>
2025-09-19 11:20:15 +0530

    Add new profile image for sub-category with ID 1758258515056

--- src/app/repository/taxClassRepository.js
+++ src/app/repository/taxClassRepository.js
@@ -80,9 +80,7 @@ class TaxClassRepository extends CrudRepository {
     }
   }
 
- 
-}
- async getActiveTaxClassesWithOptions(vendorId = null) {
+  async getActiveTaxClassesWithOptions(vendorId = null) {
     try {
       // First get active tax classes
       const filter = { isActivated: true, deletedAt: null };
@@ -122,5 +120,8 @@ class TaxClassRepository extends CrudRepository {
       throw new AppError('Failed to get active tax classes with options', StatusCodes.INTERNAL_SERVER_ERROR);
     }
   }
+ 
+}
+ 
 
 export default TaxClassRepository;
