2e54005f5cec0fdc67bd8d6d90d6c1311e81abdf
Anshul <you@example.com>
2025-12-12 17:28:01 +0530

    fix: Update API base URL to use the production endpoint

--- src/axiosConfig/axiosInstance.js
+++ src/axiosConfig/axiosInstance.js
@@ -4,7 +4,7 @@ import axios from "axios";
 
 // API base URL
 const API_BASE_URL =
-  process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000/";
+  process.env.NEXT_PUBLIC_BASE_URL || "https://diamond.nexprism.in/";
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
