cc5fbfc41d9fda7c6c7c50aa8d002a2efe7d9aca
Anshul <you@example.com>
2026-03-27 18:48:29 +0530

    fix: import useEffect in Page component for proper functionality

--- src/app/meet/page.jsx
+++ src/app/meet/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 import { X } from "lucide-react";
-import React from "react";
+import React, { useEffect } from "react";
 import CalendlyWidget from "./CalendlyWidget";
 
 function Page() {
